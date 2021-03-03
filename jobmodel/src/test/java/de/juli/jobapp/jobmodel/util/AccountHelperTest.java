0d3062d6f0ff85a21080419c2c7eb356af6cea1b
ulrich1966 <post2uli@gmx.de>
2021-03-03 15:34:58 +0100

    mouve AccountHelper again in appropiate test package

---
+++ jobmodel/src/test/java/de/juli/jobapp/jobmodel/util/AccountHelperTest.java
@@ -0,0 +1,19 @@
+package de.juli.jobapp.jobmodel.util;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AccountHelperTest {
+
+	@Test
+	public void testGetInstance() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testFillAccoundByProperties() {
+		fail("Not yet implemented");
+	}
+
+}
